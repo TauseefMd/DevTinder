a9ce1bb3d3e637a86a1b4d8cb2cbb08ac2ba82e4
Md Tauseef <tauseef426558@gmail.com>
2025-11-18 00:51:32 +0530

    Exploring get, post, delete

--- src/app.js
+++ src/app.js
@@ -2,11 +2,34 @@ const express = require("express");
 
 const app = express();
 
-app.use("/dashboard", (req, res) => {
-  res.send("Hello from the Dashboard!");
+app.use("/user", (req, res) => {
+  res.send({
+    firstName: "Md",
+    lastName: "Tauseef",
+  });
 });
 
-app.use("/", (req, res) => {
+app.get("/user", (req, res) => {
+  res.send({
+    firstName: "Md",
+    lastName: "Tauseef",
+  });
+});
+
+app.post("/user", (req, res) => {
+  res.send("User information saved successfully!");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Data deleted successfully!");
+});
+
+app.patch("/user", (req, res) => {
+  res.send("Data is patched");
+});
+
+// this will match all the HTTP method API calls to /test
+app.use("/test", (req, res) => {
   res.send("Hello from the server!");
 });
 
