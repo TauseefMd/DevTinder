6195bf3f447c3507e4b22747b56930ee5ee28cdd
Md Tauseef <tauseef426558@gmail.com>
2025-11-18 01:31:35 +0530

    Playing with routes

--- src/app.js
+++ src/app.js
@@ -2,35 +2,12 @@ const express = require("express");
 
 const app = express();
 
-app.use("/user", (req, res) => {
+app.get("/user/:userId/:name", (req, res) => {
+  console.log(req.params);
   res.send({
     firstName: "Md",
     lastName: "Tauseef",
   });
-});
-
-app.get("/user", (req, res) => {
-  res.send({
-    firstName: "Md",
-    lastName: "Tauseef",
-  });
-});
-
-app.post("/user", (req, res) => {
-  res.send("User information saved successfully!");
-});
-
-app.delete("/user", (req, res) => {
-  res.send("Data deleted successfully!");
-});
-
-app.patch("/user", (req, res) => {
-  res.send("Data is patched");
-});
-
-// this will match all the HTTP method API calls to /test
-app.use("/test", (req, res) => {
-  res.send("Hello from the server!");
 });
 
 app.listen(3000, () => {
