b35ef9db174c1621f60e272ad1bfd269e77ac7cc
Md Tauseef <tauseef426558@gmail.com>
2025-11-19 12:28:51 +0530

    init-database

--- src/app.js
+++ src/app.js
@@ -1,15 +1,32 @@
 const express = require("express");
-
 const app = express();
+const connectDB = require("./config/database");
+const User = require("./models/user");
 
-app.get("/user/:userId/:name", (req, res) => {
-  console.log(req.params);
-  res.send({
-    firstName: "Md",
-    lastName: "Tauseef",
+app.post("/signup", async (req, res) => {
+  const user = new User({
+    firstName: "Sachin",
+    lastName: "Tendulkar",
+    emailId: "sachin@gmail.com",
+    password: "sachin@123",
   });
+
+  try {
+    await user.save();
+    res.send("User added successfully!");
+  } catch (error) {
+    res.status(400).send("Error saving the user: " + error.message);
+  }
 });
 
-app.listen(3000, () => {
-  console.log("Server is successfully listening on port 3000.......");
-});
+connectDB()
+  .then(() => {
+    console.log("Successfully connected to Database....");
+    app.listen(3000, () => {
+      console.log("Server is successfully listening on port 3000.......");
+    });
+  })
+  .catch((err) => {
+    console.error("Database cannot be connected!!!!");
+    console.log(err);
+  });
