1f51c60e371fe6273ec44b02803e0adbb9428c7f
Md Tauseef <tauseef426558@gmail.com>
2025-11-30 02:19:58 +0530

    feed API + pagination

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,6 +3,7 @@ const userRouter = express.Router();
 
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 
 const USER_SAFE_DATA = "firstName lastName photoUrl age gender about skills";
 
@@ -57,7 +58,47 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
     });
   } catch (error) {
     res.status(400).json({
-      message: "Error etting connection request!!",
+      message: "Error getting connection request!!",
+      error: error.message,
+    });
+  }
+});
+
+userRouter.get("/user/feed", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const pageNum = req.query.pageNum;
+    const limit = req.query.limit;
+    const skip = (pageNum - 1) * limit;
+
+    const connectionRequest = await ConnectionRequest.find({
+      $or: [{ toUserId: loggedInUser._id }, { fromUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId");
+
+    const hideUserFromFeed = new Set();
+    connectionRequest.forEach((req) => {
+      hideUserFromFeed.add(req.fromUserId.toString());
+      hideUserFromFeed.add(req.toUserId.toString());
+    });
+
+    const feedData = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUserFromFeed) } },
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    })
+      .select(USER_SAFE_DATA)
+      .skip(skip)
+      .limit(limit);
+
+    res.json({
+      message: "Feed data fetched successfully!!!",
+      data: feedData,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "Error getting feed!!",
       error: error.message,
     });
   }
